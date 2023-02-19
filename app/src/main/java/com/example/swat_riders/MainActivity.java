e0dc31a66537218cc56a9b06db2f818ff41bb7b0
Abbas khan <abbasswat2018@gmail.com>
2023-02-20 01:05:32 +0500

    changes in MainActivity.java

--- app/src/main/java/com/example/swat_riders/MainActivity.java
+++ app/src/main/java/com/example/swat_riders/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// another change has done
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
