5b6dc0ae7a581be633da067bc895852821367844
user 2 <abbasswat2018@gmail.com>
2023-02-22 23:23:00 +0500

    added recyclerview

--- app/src/main/java/com/example/swat_riders/MainActivity.java
+++ app/src/main/java/com/example/swat_riders/MainActivity.java
@@ -3,13 +3,61 @@ package com.example.swat_riders;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.swat_riders.Adapter.Adapter;
+import com.example.swat_riders.Model.ModelClass;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
-// another change has done
-    //two
+
+    LinearLayoutManager linearLayoutManager;
+    RecyclerView recyclerView;
+    List<ModelClass>userList;
+    Adapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initData();
+        initRecyclerView();
+        
+    }
+
+    private void initData() {
+
+        userList = new ArrayList<>();
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+        userList.add(new ModelClass(R.drawable.mustafa,"Mustafa","10:00 am","how are you","________"));
+
+
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter = new Adapter(userList);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
