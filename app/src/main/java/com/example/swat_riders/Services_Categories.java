3e69c5fd2df95b5ac8db8f6f0fd5517315f790da
user 2 <abbasswat2018@gmail.com>
2023-02-22 20:36:46 +0500

    in this commit we add the service_category_activity and we done some changes in sign_up activity and in service cateogary activity and some other changes

---
+++ app/src/main/java/com/example/swat_riders/Services_Categories.java
@@ -0,0 +1,14 @@
+package com.example.swat_riders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Services_Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services_categories);
+    }
+}
