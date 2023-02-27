afffb6ed477c55e710b00229ba913081e2198c4f
user 2 <abbasswat2018@gmail.com>
2023-02-27 09:44:22 +0500

    upgrade gradle

--- app/src/main/java/com/example/swat_riders/MainActivity.java
+++ app/src/main/java/com/example/swat_riders/MainActivity.java
@@ -8,9 +8,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.swat_riders.Adapter.Adapter;
 import com.example.swat_riders.Model.ModelClass;
+import com.sagarkoli.chetanbottomnavigation.chetanBottomNavigation;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,10 +29,76 @@ public class MainActivity extends AppCompatActivity {
     List<ModelClass>userList;
     Adapter adapter;
 
+    private static final int home =1;
+    private static final int like =2;
+    private static final int map =3;
+    private static final int notificaton =4;
+    private static final int payment =5;
+
+    chetanBottomNavigation bottomNavigation;
+    TextView textView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.navbar);
+        textView = findViewById(R.id.btName);
+
+        bottomNavigation.add(new chetanBottomNavigation.Model(home,R.drawable.home));
+        bottomNavigation.add(new chetanBottomNavigation.Model(notificaton,R.drawable.bell));
+        bottomNavigation.add(new chetanBottomNavigation.Model(map, R.drawable.heart));
+        bottomNavigation.add(new chetanBottomNavigation.Model(like,R.drawable.user));
+        bottomNavigation.add(new chetanBottomNavigation.Model(payment,R.drawable.heart));
+
+        bottomNavigation.setCount(notificaton,"10");
+        bottomNavigation.setOnShowListener(new chetanBottomNavigation.ShowListener(){
+            String name;
+            @Override
+            public void onShowItem(chetanBottomNavigation.Model item) {
+
+
+                switch (item.getId()){
+                    case home:
+                        name="home";
+                        break;
+
+                    case map:
+                        name="map";
+                        break;
+                    case notificaton:
+                        name="bell";
+                        break;
+                    case payment:
+                        name="haert";
+                    case like:
+                        name="heart";
+                        break;
+                    default:
+                        name="";
+
+                }
+                textView.setText(name);
+
+            }
+        });
+        bottomNavigation.setOnClickMenuListener(new chetanBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(chetanBottomNavigation.Model item) {
+                Toast.makeText(MainActivity.this, "item closed", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        bottomNavigation.setOnReselectListener(new chetanBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(chetanBottomNavigation.Model item) {
+                Toast.makeText(MainActivity.this, "item Reselected", Toast.LENGTH_SHORT).show();
+            }
+        });
+        bottomNavigation.show(home,true);
+
 
         initData();
         initRecyclerView();
